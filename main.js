a1ca48c076dff1051e37254d95b37c6166e9ef26
DASHA <dariaepaneshnikova@icloud.com>
2019-02-15 12:20:19 +0100

    Added javascript objects'

---
+++ main.js
@@ -0,0 +1,34 @@
+const data = {
+    catdata: [
+        {
+            breed: 'Ragdoll',
+            url: 'https://i.pinimg.com/originals/41/62/c9/4162c993b0f63f8a3bd1f6a6c9dcec07.jpg'
+        },
+
+        {
+            breed: 'Bombay',
+            url: 'https://i.pinimg.com/originals/e5/d8/01/e5d801764c99bff35ec7098cac56f203.jpg'
+        },
+
+        {
+            breed: 'Munchkin',
+            url: 'http://www.abessinier.at/fotos/kitten/gross/kitten077.jpg'
+        },
+
+        {
+            breed: 'Russian Blue',
+            url: 'https://i.pinimg.com/originals/8e/d5/35/8ed535cbd7dbc55085624bb17c656656.jpg'
+        },
+
+        {
+            breed: 'Abyssinian',
+            url: 'https://cdn1.foap.com/images/98a35808-f5ca-4187-882e-0c8780c5bd69/w640.jpg?1512148480'
+        },
+
+        {
+            breed: 'Sphynx',
+            url:  'https://free-classifieds-usa.com/oc-content/uploads/858/72049.jpg'
+        }
+    
+    ]
+}
