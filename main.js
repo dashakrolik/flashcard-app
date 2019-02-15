752b44847fe693d70d6b7aead1b0d2d320c00f0a
Danielle <repko.dm@gmail.com>
2019-02-15 14:06:43 +0100

    Checking if id is same es breed for first image

--- main.js
+++ main.js
@@ -2,33 +2,67 @@ const data = {
     catdata: [
         {
             breed: 'Ragdoll',
-            url: 'https://i.pinimg.com/originals/41/62/c9/4162c993b0f63f8a3bd1f6a6c9dcec07.jpg'
+            url: 'https://free-classifieds-usa.com/oc-content/uploads/858/72049.jpg'
         },
 
         {
             breed: 'Bombay',
-            url: 'https://i.pinimg.com/originals/e5/d8/01/e5d801764c99bff35ec7098cac56f203.jpg'
+            url: 'http://www.mochablastburmese.com/yahoo_site_admin/assets/images/Katinka3.340145404_std.jpg'
         },
 
         {
             breed: 'Munchkin',
-            url: 'http://www.abessinier.at/fotos/kitten/gross/kitten077.jpg'
+            url: 'https://www.zastavki.com/pictures/640x480/2014Animals___Cats_Little_munchkin_kitten_092277_29.jpg'
         },
 
         {
             breed: 'Russian Blue',
-            url: 'https://i.pinimg.com/originals/8e/d5/35/8ed535cbd7dbc55085624bb17c656656.jpg'
+            url: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Russian_Blue_kitten.jpg'
         },
 
         {
             breed: 'Abyssinian',
-            url: 'https://cdn1.foap.com/images/98a35808-f5ca-4187-882e-0c8780c5bd69/w640.jpg?1512148480'
+            url: 'http://www.abessinier.at/fotos/kitten/gross/kitten077.jpg'
         },
 
         {
             breed: 'Sphynx',
-            url:  'https://free-classifieds-usa.com/oc-content/uploads/858/72049.jpg'
+            url:  'https://i.pinimg.com/originals/8e/d5/35/8ed535cbd7dbc55085624bb17c656656.jpg'
         }
     
     ]
 }
+
+const firstDiv = document.getElementById('ragdoll')
+firstDiv.onclick = function() {
+
+const firstCatId = document.getElementById('ragdoll')
+const firstCatBreed = data.catdata[0].breed
+
+console.log(firstCatId.innerText, firstCatBreed);
+
+if (firstCatId.innerText === firstCatBreed) {
+    console.log('doei')
+    
+}
+
+// const firstCatBreed = data.catdata[0].breed
+// const firstCatUrl = data.catdata[0].url
+
+// const firstImage = document.createElement('img')
+// firstImage.src = firstCatUrl
+
+// firstDiv.appendChild(firstImage)
+
+
+
+}
+
+// const catArray = data.catdata
+
+// catdata.array.forEach(catObject => {
+
+
+
+    
+// });
